f5e4855a347e877442be734460d807e9820812cb
sangoh <jaws0910@gmail.com>
2022-05-15 17:13:25 +0900

    create project QueryDSL

---
+++ querydsl/src/main/kotlin/com/yeh35/springjpaprectice/querydsl/QuerydslApplication.kt
@@ -0,0 +1,11 @@
+package com.yeh35.springjpaprectice.querydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuerydslApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuerydslApplication>(*args)
+}
